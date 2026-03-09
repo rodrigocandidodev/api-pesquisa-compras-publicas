431591a644266b7eb6528cc2138dcecd29f08ca5
Rodrigo Candido da Silva <rodrigo_candido.contato@outlook.com>
2026-03-09 17:51:01 -0300

    FEAT: adicionar codigoModalidadeContratacao ao schema

--- src/schemas/consultaContratacoesPorDataPublicacaoSchema.ts
+++ src/schemas/consultaContratacoesPorDataPublicacaoSchema.ts
@@ -4,6 +4,7 @@ export const ConsultaContratacoesPorDataPublicacaoSchema = z.object({
   dataInicial: z.string({ message: "Data inicial obrigatória!" }),
   dataFinal: z.string({ message: "Data final obrigatória!" }),
   pagina: z.number({ message: "Página obrigatória!" }),
+  codigoModalidadeContratacao : z.number({ message: "Modalidade de Contratação obrigatória!" }),
   tamanhoPagina: z.number({ message: "Tamanho da página obrigatório!" }),
 });
 
